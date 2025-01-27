402e2eec24f8ad0b195651936f110679aae2ee0c
saminame <sesteqla@ucsc.edu>
2025-01-27 12:33:48 -0800

    added name and date

--- experiment3/js/project.js
+++ experiment3/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Samina Esteqlal
+// Date: January 27, 2025
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
